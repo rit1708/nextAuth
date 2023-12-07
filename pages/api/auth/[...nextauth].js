10c8601d84814503668c573d4958f3f1bd6bb43c
ritesh <ritesh@thoughtwin.com>
2023-12-07 17:11:15 +0530

    third commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,13 @@ export const authOptions = {
   providers: [
     AppleProvider({
       clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_PRIVATE_KEY,
+      clientSecret: {
+        appleId: process.env.APPLE_ID,
+        privateKey: process.env.APPLE_PRIVATE_KEY,
+        // teamId: process.env.APPLE_TEAM_ID,
+        // keyId: process.env.APPLE_KEY_ID,
+      },
+      redirectURI: 'http://localhost:3000/',
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -27,9 +33,13 @@ export const authOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    session: async ({ session }) => {
-      session.customValue = new Date().toISOString();
-      return Promise.resolve(session);
+    async redirect(url, baseUrl) {
+      // Ensure that url is a string before using startsWith
+      const redirectUrl = typeof url === 'string' ? url : baseUrl;
+      
+      // Customize the redirect URL after sign-in
+      // Example: Redirect to the home page of your site
+      return redirectUrl;
     },
   },
 }
